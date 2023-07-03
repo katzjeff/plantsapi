05d0b6876e6534762ee3f25d4dfec3270a643274
Jeffrey <aligula.jeff@gmail.com>
2023-07-03 10:49:38 +0300

    update: deleteRoute so as to delete based on ID

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -1,55 +1,21 @@
 import Plant from "../models/allPlantsModel.js";
 
 export const deletePlant = (req, res) => {
-  const identifier = req.params.id;
-  console.log(identifier);
-  const searchBy = isNaN(identifier) ? "plantName" : "id";
-  console.log(searchBy);
+  const plantId = req.params.id;
 
-  let query;
-  if (searchBy === "plantName") {
-    query = { plantName: identifier };
-  } else {
-    query = { id: parseInt(identifier) };
-  }
-
-  console.log(query);
-
-  Plant.findOneAndRemove(query)
+  Plant.findByIdAndDelete(plantId)
     .then((deletedPlant) => {
       if (!deletedPlant) {
         return res.status(404).json({ error: `Sorry, plant not found.` });
       }
-      res.json({ message: `Plant has been deleted successfully.` });
+
+      const plantName = deletedPlant.plantName;
+      res.json({
+        message: `Plant ${plantName} has been deleted successfully.`,
+      });
     })
     .catch((error) => {
       console.error(error);
       res.status(500).json({ error: "Failed to delete the plant." });
     });
 };
-
-// export const deletePlant = (req, res) => {
-//   const identifier = req.params.id;
-//   const plantName = req.params.plantName
-//   const searchBy = isNaN(identifier) ? "plantName" : "id";
-
-//   let query;
-
-//   if (searchBy === "id") {
-//     query = { id: parseInt(identifier) };
-//   } else {
-//     query = { plantName: plantName };
-//   }
-
-//   Plant.findOneAndDelete(query)
-//     .then((deletedPlant) => {
-//       if (!deletedPlant) {
-//         return res.status(404).json({ error: `Sorry, plant not found.` });
-//       }
-//       res.json({ message: ` Plant has been deleted successfully.` });
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       res.status(500).json({ error: "Failed to delete the plant." });
-//     });
-// };
