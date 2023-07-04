e908bdb34eec0a9f699fd47dcb7c6327092a89f8
Jeffrey <aligula.jeff@gmail.com>
2023-07-04 09:47:06 +0300

    updated userModel: removed email match

--- models/userModel.js
+++ models/userModel.js
@@ -5,9 +5,6 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
-    // unique: true,
-    // match:
-    //   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
   },
   password: { type: String, required: true, unique: true },
 });
