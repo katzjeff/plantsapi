01e2ac4364531b7ebe45b3c4e6203f1733335426
Jeffrey <aligula.jeff@gmail.com>
2023-07-20 15:43:40 +0300

    removed user model file

--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  userName: { type: String },
-  email: {type: String},
-  password: { type: String },
-});
-
-export default mongoose.models?.Users || mongoose.model("User", userSchema);
